79192d2ec8bdb37e7c86b314199d33a6fb44e292
phl3bas <phl3bas87@gmail.com>
2022-09-26 10:37:24 +0100

    init commit

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import esbuild from 'rollup-plugin-esbuild'
+
+export default {
+    input: 'src/index.js',
+    output: {
+      file: 'bundle.js',
+      format: 'cjs'
+    },
+    plugins: [
+        esbuild({
+          exclude: /node_modules/, // default
+          minify: true ,
+          target: 'es2015', // default, or 'es20XX', 'esnext'
+          loaders: {
+            // Add .json files support
+            // require @rollup/plugin-commonjs
+            '.json': 'json',
+            // Enable JSX in .js files too
+            '.js': 'jsx',
+          },
+        }),
+      ],
+  };
---
+++ src/index.js
@@ -0,0 +1,15 @@
+const ONE  = 1
+const TWO = 2
+
+
+export function sum(a, b) {
+    return a + b
+}
+
+export function addOne(a) {
+    return a + ONE
+}
+
+export function addTwo(a) {
+    return a + TWO
+}
